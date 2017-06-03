83529024555852dd106e3f06c25b5764720cf786
James Liu <james_liu@fastmail.com>
2017-06-03 19:16:13 +1000

    added Saved Flights button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Divider } from 'semantic-ui-react'
 import './Sidebar.css'
 
 import SearchForm from './SearchForm'
@@ -9,6 +10,12 @@ class Sidebar extends React.Component {
       <div className="sidebar">
         <h1 className="header-text">airborne</h1>
         <SearchForm />
+        
+        <Divider inverted />
+
+        <div className="saved-flights-link">
+          <Button color='red' fluid>Saved Flights</Button>
+        </div>
       </div>
     )
   }
