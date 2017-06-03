110c8bd8a32cc37aed84b7c0b41ef30964c57ff8
James Liu <james_liu@fastmail.com>
2017-06-03 13:53:46 +1000

    created basic page layout

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import './App.css';
+import Home from './Home.js';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={ Home } />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+import './Home.css'
+
+const Home = () => (
+  <div className="wrapper">
+    <div className="sidebar" />
+    <div className="main-content" />
+  </div>
+)
+
+export default Home
