834c051d458744dba5f1d834dd33516ba47dcefc
James Liu <james_liu@fastmail.com>
2017-06-03 17:40:45 +1000

    new date picker and sidebar styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,35 +1,67 @@
 import React from 'react'
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Form, Dropdown, Divider } from 'semantic-ui-react'
 import SearchExampleStandard from './AirportField'
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import "react-day-picker/lib/style.css"
+import "./SearchForm.css"
 import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 var moment = require('moment');
 
 
 class SearchForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      startDate: moment(),
-      endDate: moment().add(7, 'days'),
-      focused: false
-    }
+  state = {
+    startDate: null,
+    endDate: null,
   }
   render() {
+    const options = [
+      { key: 0, text: '0', value: 0 },
+      { key: 1, text: '1', value: 1 },
+      { key: 2, text: '2', value: 2 },
+      { key: 3, text: '3', value: 3 },
+      { key: 4, text: '4', value: 3 },
+      { key: 5, text: '5', value: 3 },
+      { key: 6, text: '6', value: 3 },
+    ]
     return (
       <div>
         <Form>
+          <Divider horizontal inverted>Where in the World</Divider>
+
           <Form.Field>
-            <SearchExampleStandard fluid={true} size="large" placeholder="from"/>
+            <SearchExampleStandard fluid size="large" placeholder="from"/>
           </Form.Field>
           <Form.Field>
-            <SearchExampleStandard fluid={true} size="large" placeholder="to"/>
+            <SearchExampleStandard fluid size="large" placeholder="to"/>
           </Form.Field>
-          <Form.Field>
-            <Checkbox label='returning?' />
+
+          <Divider horizontal inverted>Dates</Divider>
+
+          <Form.Field width="16">
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+              startDatePlaceholderText="departing"
+              endDatePlaceholderText="returning"
+            />
           </Form.Field>
-          <Form.Field>
-          </Form.Field>
-          <Button type='submit' fluid={true}>takeoff!</Button>
+
+          <Divider horizontal inverted>Passengers</Divider>
+
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <Dropdown placeholder='adults' compact selection options={options} />
+            </Form.Field>
+            <Form.Field>
+              <Dropdown placeholder='children' compact selection options={options} />
+            </Form.Field>
+          </Form.Group>
+          
+          <Button inverted type='submit' fluid>TAKEOFF!</Button>
         </Form>
       </div>  
     )
